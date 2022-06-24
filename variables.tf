f9a10d0f7f53cdd5131b5bf44f466bccff922a4a
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2022-06-24 08:30:40 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,6 @@
 variable "accessKey" {}  
 variable "secretKey" {}
-variable "AWSregion" {
-    default = "us-east-1"
-}
+variable "AWSRegion" {}
 variable "instance_name" {
     type = string
     default = "testvm"
