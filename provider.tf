b761754c7ab8a52df98f19c1417351e0f7d827f8
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2022-06-24 10:05:14 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
-    region = var.AWSregion
+    region = var.region
     access_key = var.accessKey
     secret_key = var.secretKey
+    profile = "default"
   }
