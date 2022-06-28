8fafdb8644de185b86c390542b9ebe0b562674d2
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2022-06-28 14:45:14 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
+variable "accessKey" {}  
+variable "secretKey" {}
 variable "instance_name" {
     type = string
-    default = "testvm"
   }
   
   variable "sn_instance" {
     type = string
     default = "DoNotModify"
   }
-  
